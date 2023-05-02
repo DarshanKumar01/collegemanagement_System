3fcf24e8956eed7f9ed395368dd57b43a586b161
Darshan_Kumar_P_E <darshankumar2332@gmail.com>
2023-05-02 15:07:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-100 in-ad-ap">
+			
+		</div>
+		<div class="paragraph">
+			<p>
+				USMR provides a harmonious environment and learning opportunities  to its students regardless of their gender, socioeconomic background, religious beliefs and regional differences.
+			</p>
+		</div>
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
+
